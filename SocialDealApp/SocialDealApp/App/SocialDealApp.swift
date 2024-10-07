0e7cbd979c9782709bcdbd6fbb20d12e38e02d64
katjaryzhkova <e.ryzhkova@student.avans.nl>
2024-10-07 19:27:51 +0200

    chore: project setup

---
+++ SocialDealApp/SocialDealApp/App/SocialDealApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+/**
+ The application's base class which initializes the app.
+ */
+@main
+struct SocialDealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
